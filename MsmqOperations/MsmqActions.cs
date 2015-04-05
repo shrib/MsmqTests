a3ee9d5a5bd4972c353504299f4dfd051874a022
shrib <sbhupathi@outlook.com>
2015-04-05 11:41:48 -0400

    Added msmq actions.

---
+++ MsmqOperations/MsmqActions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Messaging;
+using System.Diagnostics;
+
+namespace Webhive.Blog.MsmqOperations {
+    class MsmqActions {
+        public static void CreateQueue(string queueName) {
+            if (MessageQueue.Exists(queueName)) { Console.WriteLine("Queue '{0}' already exists.", queueName); 
+            }
+
+            try {
+                var queue = MessageQueue.Create(queueName);
+                queue.Dispose();
+
+                Console.WriteLine("Private queue '{0}' successfully created.", queueName);
+            }
+            catch (MessageQueueException ex) {
+                Console.WriteLine("Error creating queue '{0}':\n{1}", queueName, ex);
+            }
+        }
+
+        public static void EnqueueMessages<T>(string queueName, T message, int numberOfMessages) {
+            var stopwatch = Stopwatch.StartNew();
+            var queue = new MessageQueue(queueName) { Formatter = new BinaryMessageFormatter() };
+
+            try {
+                for (var index = 0; index < numberOfMessages; ++index) {
+                    var msg = new Message(message) { Formatter = new BinaryMessageFormatter() };
+                    queue.Send(msg);
+                }
+
+                stopwatch.Stop();
+                Console.WriteLine("Successfully enqueued '{0}' messages in '{1}' milliseconds.", numberOfMessages, stopwatch.ElapsedMilliseconds);
+            }
+            catch (MessageQueueException ex) {
+                Console.WriteLine("Error enqueuing messages: {0}", ex);
+            }
+
+            queue.Dispose();
+        }
+
+        public static void PurgeQueue(string queueName) {
+            var queue = new MessageQueue(queueName) { Formatter = new BinaryMessageFormatter() };
+
+            try {
+                queue.Purge();
+                Console.WriteLine("Successfully purged '{0}'.", queueName);
+            }
+            catch (MessageQueueException ex) {
+                Console.WriteLine("Error purging {0}:\n{1}", queueName, ex);
+            }
+
+            queue.Dispose();
+        }
+
+        public static void DequeueMessages(string queueName) {
+            //
+        }
+    }
+}
--- MsmqOperations/Program.cs
+++ MsmqOperations/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace MsmqOperations {
+namespace Webhive.Blog.MsmqOperations {
     class Program {
-        static void Main(string[] args) {
+        private static void Main() {
+            PromptForAction();
+
+            Console.ReadLine();
+        }
+
+        private static void PromptForAction() {
+            var optionSelected = "-1";
+
+            Console.WriteLine("1. Create a queue");
+            Console.WriteLine("2. Purge messages from a queue");
+            Console.WriteLine("3. Enqueue messages");
+            Console.WriteLine("4. Dequeue messages");
+            Console.WriteLine("5. Delete queue");
+            Console.WriteLine("0. Exit");
+
+            while (!optionSelected.Equals("0")) {
+                optionSelected = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(optionSelected))
+                    optionSelected = "";
+                else
+                    DoAction(optionSelected);
+            }
+        }
+
+        private static void DoAction(string optionSelected) {
+            switch (optionSelected) {
+                case "1":
+                    Program.CreateQueue();
+                    break;
+            }
+        }
+
+        private static void CreateQueue() {
+            Console.WriteLine("Queue Name: ");
+            var queueName = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(queueName))
+                Console.WriteLine("Invalid queue name specified. Aborting operation.");
+            else
+                MsmqActions.CreateQueue(queueName);
         }
     }
 }
