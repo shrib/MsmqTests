1dc36349d544b2b9473f0df611f86e2481805605
Shri Bhupathi <sbhupathi@outlook.com>
2015-06-12 14:57:35 -0400

    Update Program.cs

--- MsmqOperations/Program.cs
+++ MsmqOperations/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+using System;
 
 namespace Webhive.Blog.MsmqOperations {
     class Program {
+        static string _queueName = "";
         private static void Main() {
+            Console.Write("Enter queue name: ");
+            _queueName = Console.ReadLine();
+
             PromptForAction();
 
             Console.ReadLine();
@@ -11,12 +15,12 @@ namespace Webhive.Blog.MsmqOperations {
         private static void PromptForAction() {
             var optionSelected = "-1";
 
-            Console.WriteLine("1. Create a queue");
-            Console.WriteLine("2. Purge messages from a queue");
-            Console.WriteLine("3. Enqueue messages");
-            Console.WriteLine("4. Dequeue messages");
-            Console.WriteLine("5. Delete queue");
+            Console.WriteLine("1. Enqueue messages");
+            Console.WriteLine("2. Dequeue-Bulk messages");
+            Console.WriteLine("3. Dequeue-Sequential messages");
+            Console.WriteLine("4. Purge messages from a queue");
             Console.WriteLine("0. Exit");
+            Console.WriteLine();
 
             while (!optionSelected.Equals("0")) {
                 optionSelected = Console.ReadLine();
@@ -29,21 +33,74 @@ namespace Webhive.Blog.MsmqOperations {
         }
 
         private static void DoAction(string optionSelected) {
+            Console.WriteLine();
+
             switch (optionSelected) {
                 case "1":
-                    Program.CreateQueue();
+                    EnqueueMessages();
                     break;
+
+                case "2":
+                    DequeueMessages();
+                    break;
+
+                case "3":
+                    DequeueMessages(false);
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+
+        static void EnqueueMessages() {
+            Console.Write("Message count: ");
+            var input = Console.ReadLine();
+            var count = int.Parse(input ?? "0");
+            Console.Write("Formatter (1 = Binary, 2 = Xml, 3 = ActiveX): ");
+            input = Console.ReadLine();
+            var selectedFormatter = int.Parse(input ?? "1");
+
+            // NOTE: May clog the local desktop if we're just queueing a lot of messages without.
+            count = Math.Min(count, 100000);
+
+            var data = Guid.NewGuid().ToString();
+            data = string.Format("{0},{0},{0},{0}", data);
+
+            MsmqActions
+                .WithFormatter(GetFormatter(selectedFormatter))
+                .EnqueueMessages(_queueName, data, count);
+        }
+
+        static void DequeueMessages(bool bulk = true) {
+            Console.Write("Formatter (1 = Binary, 2 = Xml, 3 = ActiveX): "); // Xml doesn't support Bulk dequeue in this version
+            var input = Console.ReadLine();
+            var selectedFormatter = int.Parse(input ?? "1");
+
+            var act = new Action<string>(x => { /* Do something here with the dequeued message */ });
+
+            if (bulk) {
+                MsmqActions
+                    .WithFormatter(GetFormatter(selectedFormatter))
+                    .DequeueMessagesBulk(_queueName, act);
+            }
+            else {
+                MsmqActions
+                    .WithFormatter(GetFormatter(selectedFormatter))
+                    .DequeueMessagesSequential(_queueName, act);
             }
         }
 
-        private static void CreateQueue() {
-            Console.WriteLine("Queue Name: ");
-            var queueName = Console.ReadLine();
+        static IMessageFormatterCreator GetFormatter(int selection) {
+            switch (selection) {
+                case 2:
+                    return new XmlMessageFormatterCreator();
 
-            if (string.IsNullOrWhiteSpace(queueName))
-                Console.WriteLine("Invalid queue name specified. Aborting operation.");
-            else
-                MsmqActions.CreateQueue(queueName);
+                case 3:
+                    return new ActiveXMessageFormatterCreator();
+
+                default:
+                    return new BinaryMessageFormaterCreator();
+            }
         }
     }
 }
